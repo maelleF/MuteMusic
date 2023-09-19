d5e905b87b49ce47ce451d483eb238e86281ecb3
maelleF <maelle.freteault@gmail.com>
2023-09-19 13:55:28 -0400

    added jupyter notebook to generate mini playlists

--- letzgo.py
+++ letzgo.py
@@ -1,14 +1,47 @@
 import os
 import numpy as np
+from psychopy import visual, sound
 
-#Global Variables
+#Global Variables if multiples tasks
 
 #class MuteMusic(Task):
 
-#Class Global Variables
+#Global Variables to the Task
 
-DEFAULT_INSTRUCTION = """"""
+DEFAULT_INSTRUCTION = """Listen to the following track"""
+AUDITORY_IMAGERY_ASSESSMENT = """During the silences, did you imagine the missing part of the different audio tracks you’ve heard ?"""
+FAMILIARITY_ASSESSMENT = """Did you recognise the song ?"""
+
+#task : 1 run = 1 playlist = around 10 audio tracks
+
+#repeat for n songs in playlist_subX_runXX.txt/csv :
+#   step 1: DEFAULT_INSTRUCTION
+#   step 2: display audio
+#   step 3: Auditory imagery assessment
+#   step 4: Familiarity assessment
 
 def __init__():
-    
+    pass
  
+def _instructions():
+    pass
+
+def _run():
+    pass
+
+def _setup():
+    pass
+
+def _stop():
+    pass
+
+def _save():
+    pass
+
+def _restart():
+    pass
+
+if __name__=="__main__":
+    exp_win = visual.Window()
+    track = sound.Sound()
+    exp_win.close()
