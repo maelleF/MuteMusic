5d6d4fab62a240fb4a1480ea714fcd28b8292c3d
maelleF <maelle.freteault@gmail.com>
2023-11-06 00:13:08 -0500

    cleaning files

--- StandAlone_MuteMusic.py
+++ StandAlone_MuteMusic.py
@@ -3,7 +3,7 @@ from psychopy import visual
 from Task_MuteMusic import Playlist_StandAlone
 
 exp_win = visual.Window(units="height", allowStencil=True)
-playlist_path = './subXX_runXX.csv'
+playlist_path = './data/subXX_runXX.csv'
 
 run = Playlist_StandAlone(playlist_path, exp_win)
 run.run()
